38fb93c6ce1d9e46b42bbc2eb6b95952c0bbd278
zerlenzhang <zerlenzhang@outlook.com>
2020-11-18 22:42:13 +0800

    update debug script and regenerate MobaServer.exe

--- Server/apps/lua_test/scripts/Debug/Debug.lua
+++ Server/apps/lua_test/scripts/Debug/Debug.lua
@@ -1,44 +1,12 @@
---初始化日志模块
-Debug.LogInit("Test","Debug",true);
-
-Debug.Log("hello","???");
-
-local Player={
-	name="Player"
-}
-
-function Player:New(instant)
-	if not instant then
-		instant={};
-	end
-	setmetatable(instant, self)
-	self.__index = self
-
---	self.name="P-1";
-
-	return instant;
-end
-
-function Player:Test()
-	print("Player_Test");
-end
-
-local RedPlayer=Player:New();
-
---function RedPlayer:New(instant)
---	if not instant then
---		instant=Player:New(instant);
---	end
---	setmetatable(instant, self)
---	self.__index = self
---	return instant;
---end
-
-function RedPlayer:Test()
-	Player.Test(self);
-	print("dgasdg");
-end
-
-local p = RedPlayer:New();
-p:Test();
-print(p.name);
+Debug.LogInit("ClientLogs","client",true);
+Debug.Log("hello");
+Netbus.TcpConnect("121.196.178.141",6080,
+		--链接成功的回调
+        function(session)
+            if session then
+                print("succeed to connect to [121.196.178.141:6080]");
+                
+                return;
+            end
+            print("failed to connect to [121.196.178.141:6080]")
+        end);
